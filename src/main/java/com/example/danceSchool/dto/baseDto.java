2834e10661f336ddba3fc00853b72c021a90c5a1
Koeco97 <komissarovaolesya@yandex.ru>
2020-06-14 17:08:59 +0300

    baseDto added, other dto extend base

---
+++ src/main/java/com/example/danceSchool/dto/baseDto.java
@@ -0,0 +1,18 @@
+package com.example.danceSchool.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+public class baseDto implements Serializable {
+    @JsonProperty(value = "id")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
